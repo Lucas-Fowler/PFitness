5273a89b719fcf365f352ac8243a457a63857f68
1935539 <lafsd8565@gmail.com>
2024-03-28 09:40:46 -0700

    added the planet fitness website using a webview and also a virtual tour using a videoview

--- app/src/main/java/com/example/demo1/Tutorials.kt
+++ app/src/main/java/com/example/demo1/Tutorials.kt
@@ -2,16 +2,42 @@ package com.example.demo1
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.ImageButton
+import android.widget.MediaController
+import android.widget.VideoView
+
 
 class Tutorials : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+    lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorials)
         getSupportActionBar()?.hide()
+
+        webView = findViewById<WebView>(R.id.web) as WebView
+
+        webView.loadUrl("https://www.planetfitness.com/")
+
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = WebViewClient()
+
+        webView.settings.setSupportZoom(true)
+
+        val videoView = findViewById<VideoView>(R.id.video)
+        val packageName = "android.resource://" + getPackageName() + "/" + R.raw.tour
+        val uri = Uri.parse(packageName)
+        videoView.setVideoURI(uri)
+        videoView.start()
+//        val mediaController = MediaController(this)
+//        videoView.setMediaController(mediaController)
+
 
         val homebutton : ImageButton = findViewById(R.id.homebtn)
         val tutorialsbutton : ImageButton = findViewById(R.id.tutorialbtn)
@@ -28,6 +54,14 @@ class Tutorials : AppCompatActivity() {
         securebutton.setOnClickListener {
             intent = Intent(this, MembersOnly::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            finish()
         }
     }
 }
